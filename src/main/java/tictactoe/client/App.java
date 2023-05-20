9b049d56d520f6e714eb626ff9e0dc1e945315e4
davi-i <josedavifrancelino@gmail.com>
2023-05-20 14:30:05 -0300

    feat: used use ack

--- src/main/java/tictactoe/client/App.java
+++ src/main/java/tictactoe/client/App.java
@@ -3,14 +3,20 @@ package tictactoe.client;
 import java.net.URISyntaxException;
 import java.util.Scanner;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import io.socket.client.Ack;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
 public class App {
+  private static Socket socket;
+
   public static void main(String[] clArgs) {
     try {
-      Socket socket = IO.socket("http://localhost:9092");
+      socket = IO.socket("http://localhost:9092");
       socket.on(Socket.EVENT_CONNECT, (args) -> {
         // System.out.println("Connected to server");
         // Perform actions upon successful connection
@@ -28,17 +34,40 @@ public class App {
       // Connect to the server
       socket.connect();
 
-      Scanner scanner = new Scanner(System.in);
+      enterGame();
 
-      System.out.print("Enter yout name: ");
-      String name = scanner.nextLine();
+    } catch (
 
-      socket.emit("entergame", name);
-
-      // Send a message to the server
-      socket.emit("chatevent", "Hello, server!");
-    } catch (URISyntaxException e) {
+    URISyntaxException e) {
       e.printStackTrace();
     }
+  }
+
+  private static void enterGame() {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter yout name: ");
+    String name = scanner.nextLine();
+
+    socket.emit("entergame", name, (Ack) args -> {
+      JSONObject response = (JSONObject) args[0];
+      System.out.println(response);
+      try {
+        String result = response.getString("result");
+        if (result.equals("player1")) {
+          System.out.println("You are player 1, waiting for other player...");
+        } else if (result.equals("player2")) {
+          System.out.println("You are player 2, the game is about to start.");
+        } else if (result.equals("nameAlreadyUsed")) {
+          System.out.println("This name is already used");
+          enterGame();
+        } else if (result.equals("roomFull")) {
+          System.out.println("The room is full");
+        }
+      } catch (JSONException e) {
+      }
+    });
+
+    scanner.close();
   }
 }
--- src/main/java/tictactoe/server/App.java
+++ src/main/java/tictactoe/server/App.java
@@ -2,6 +2,8 @@ package tictactoe.server;
 
 import java.util.Optional;
 
+import org.json.JSONObject;
+
 import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOClient;
@@ -27,15 +29,30 @@ public class App {
             @Override
             public void onData(SocketIOClient client, String name, AckRequest ackRequest) {
 
-                if (player1.isEmpty()) {
-                    player1 = Optional.of(new Player(client, name));
-                    client.sendEvent("entergame", name);
-                } else if (player2.isEmpty()) {
-                    player2 = Optional.of(new Player(client, name));
-                    player1.get().getSocket().sendEvent("opponentEntered", name);
-                } else {
-                    client.sendEvent("roomFull");
+                JSONObject response = new JSONObject();
+                try {
+                    if (player1.isEmpty()) {
+                        player1 = Optional.of(new Player(client, name));
+                        response.put("result", "player1");
+                    } else if (player2.isEmpty()) {
+                        if (name == player1.get().getName()) {
+                            response.put("result", "nameAlreadryUsed");
+                        } else {
+                            player2 = Optional.of(new Player(client, name));
+                            player1.get().getSocket().sendEvent("opponentEntered", name);
+                            response.put("result", "player2");
+
+                            JSONObject players = new JSONObject();
+                            players.put("player1", player1.get().getName());
+                            players.put("player2", name);
+                            server.getBroadcastOperations().sendEvent("players", players);
+                        }
+                    } else {
+                        response.put("result", "roomFull");
+                    }
+                } catch (Exception e) {
                 }
+                ackRequest.sendAckData(new Object[] { (Object) response });
             }
         });
 
